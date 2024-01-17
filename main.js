fb3d520edcd1807f2c0fc194a7ec8066fd238a22
karthik <karthikmj351@gmail.com>
2024-01-17 23:08:11 +0530

    added some feauters 1:api intersection 2:contact section

--- main.js
+++ main.js
@@ -14,7 +14,39 @@ navLinks.forEach((link) => {
 menubarBtn.addEventListener("click",handleMenuBar);
 
 
-// TODO : 
-// Smooth scrolling effect while transitioning different section
-// Add intersection observer API effects in services section
-// Add Slider component for project-card preview 
+/* TODO : */
+/* Smooth scrolling effect while transitioning different section */
+/* contact sales and browse project smooth scrool */
+ document.getElementById('hero-btn').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.querySelector('#contact').scrollIntoView({ behavior: 'smooth' });
+  });
+  
+  document.getElementById('our-projects').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.querySelector('#projects').scrollIntoView({ behavior: 'smooth' });
+  }); 
+ /* nav smooth scrooling*/
+ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+/* // Add intersection observer API effects in services section */
+
+const serviceCards = document.querySelectorAll('.service-card');
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('in-view');
+    }else{
+        entry.target.classList.remove('in-view');
+    }
+  });
+}, { threshold: 0});  
+serviceCards.forEach(card => observer.observe(card));
+/* // Add Slider component for project-card preview  */
