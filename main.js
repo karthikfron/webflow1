bb1b7729c01e5f624bfae09468078a86697acb2b
Santhosh <pitakasanthosh@gmail.com>
2024-01-17 17:35:27 +0530

    Add formspree to contact section & adjusted styles to remove overflow

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const menubarBtn = document.querySelector("#menubar-btn")
 const navLinks = document.querySelectorAll("nav > ul > li > a")
+const contactSubmitBtn = document.querySelector("#contact-submit-btn")
 
 function handleMenuBar(){
     const navListItems = document.querySelector("header > nav > ul");
@@ -12,8 +13,8 @@ navLinks.forEach((link) => {
 
 menubarBtn.addEventListener("click",handleMenuBar);
 
+
 // TODO : 
 // Smooth scrolling effect while transitioning different section
 // Add intersection observer API effects in services section
 // Add Slider component for project-card preview 
-// Update Contact section by adding a Email service
